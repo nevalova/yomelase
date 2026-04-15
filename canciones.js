a04961c4c3421574678cbe7afea361cceee0c4c4
nevalova <terrasnet1@gmail.com>
2026-04-14 18:35:42 -0600

    Add files via upload

---
+++ canciones.js
@@ -0,0 +1,20 @@
+const CANCIONES = [
+  {
+    titulo: "Billie Jean",
+    artista: "Michael Jackson",
+    year: 1982,
+    spotifyId: "5ChkMS8OtdzJeqyybCc9R5"
+  },
+  {
+    titulo: "Smells Like Teen Spirit",
+    artista: "Nirvana",
+    year: 1991,
+    spotifyId: "5ghIJDpPoe3CfHMGu71E6T"
+  },
+  {
+    titulo: "Wonderwall",
+    artista: "Oasis",
+    year: 1995,
+    spotifyId: "7ygpwy2qP3NbrxVkHvUhXY"
+  }
+];
